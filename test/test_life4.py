286cb50288ccba1b66e95184701501b2dae1c8a8
Rob van der Most <Silvester747@gmail.com>
2017-08-28 11:14:26 +0200

    [Life4] Count alive neighbours.

--- test/test_life4.py
+++ test/test_life4.py
@@ -1,8 +1,15 @@
 import pytest
 
+DEAD = 0
+ALIVE = 1
+
 
 def is_alive(neighbour_count):
     return neighbour_count in [2, 3]
+
+
+def count_alive_neighbours(neighbours):
+    return sum(neighbours)
 
 
 @pytest.mark.parametrize(
@@ -21,3 +28,15 @@ def is_alive(neighbour_count):
 )
 def test_is_alive(neighbour_count, expected_alive):
     assert is_alive(neighbour_count) == expected_alive
+
+
+@pytest.mark.parametrize(
+        "neighbours, expected_alive_count",
+        (
+            ([DEAD]*8, 0),
+            ([ALIVE]*8, 8),
+            ([DEAD, ALIVE]*4, 4),
+        )
+    )
+def test_count_alive_neighbours(neighbours, expected_alive_count):
+    assert count_alive_neighbours(neighbours) == expected_alive_count
