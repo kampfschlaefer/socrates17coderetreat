34e18c3510c729cb6e293c0402288910daec3900
Rob van der Most <Silvester747@gmail.com>
2017-08-28 11:32:39 +0200

    [Life4] Get neighbouring cells from board.

--- test/test_life4.py
+++ test/test_life4.py
@@ -12,6 +12,20 @@ def count_alive_neighbours(neighbours):
     return sum(neighbours)
 
 
+"""
+Get the neighbours for the cell at (x, y).
+
+No wrap-around at the borders!
+"""
+def get_neighbors(board, x, y):
+    neighbours = []
+    for i in range(max(0, x-1), min(x+2, len(board))):
+        for j in range(max(0, y-1), min(y+2, len(board[i]))):
+            if (i, j) != (x, y):
+                neighbours.append(board[i][j])
+    return neighbours
+
+
 @pytest.mark.parametrize(
         "neighbour_count, expected_alive",
         (
@@ -40,3 +54,42 @@ def test_is_alive(neighbour_count, expected_alive):
     )
 def test_count_alive_neighbours(neighbours, expected_alive_count):
     assert count_alive_neighbours(neighbours) == expected_alive_count
+
+
+neighbour_test_board = [
+                            [DEAD, ALIVE, ALIVE, DEAD],
+                            [ALIVE, DEAD, ALIVE, DEAD],
+                            [ALIVE, ALIVE, DEAD, DEAD],
+                            [DEAD, ALIVE, DEAD, ALIVE]
+                        ]
+@pytest.mark.parametrize(
+        "board, x, y, expected_neighbours",
+        (
+            ([[DEAD]*10 for _ in range(12)],
+             1, 1,
+             [DEAD]*8),
+            (neighbour_test_board[:],
+             1, 1,
+             [DEAD, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, ALIVE, DEAD]),
+            (neighbour_test_board[:],
+             2, 2,
+             [DEAD, ALIVE, DEAD, ALIVE, DEAD, ALIVE, DEAD, ALIVE]),
+            (neighbour_test_board[:],
+             3, 0,
+             [ALIVE, ALIVE, ALIVE]),
+            (neighbour_test_board[:],
+             2, 0,
+             [ALIVE, DEAD, ALIVE, DEAD, ALIVE]),
+            (neighbour_test_board[:],
+             0, 3,
+             [ALIVE, ALIVE, DEAD]),
+            (neighbour_test_board[:],
+             1, 3,
+             [ALIVE, DEAD, ALIVE, DEAD, DEAD]),
+            (neighbour_test_board[:],
+             0, 2,
+             [ALIVE, DEAD, DEAD, ALIVE, DEAD]),
+        )
+    )
+def test_get_neighbors(board, x, y, expected_neighbours):
+    assert get_neighbors(board, x, y) == expected_neighbours
